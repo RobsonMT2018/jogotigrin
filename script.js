22c8a972526502ea73952bbbd4d2fb756158d200
robsonmt <42287752+RobsonMT2018@users.noreply.github.com>
2024-05-05 19:20:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
     let placar = 0;
-    let contagem = 15;
+    let CreditoAdicional = 0;
     let pontos = 0;
-    let coin = 0;
+    let AdicionarCredito = 0;
+    let multCredito = 0;
 
 
 function getRandomInt(max , min) {
@@ -16,113 +17,137 @@ function getRandomInt(max , min) {
 
 function addficha(){
 
-    coin = document.getElementById("ficha").value;
-    contagem = contagem * coin;
-    document.getElementById("apost1").textContent = "Ficha " + coin;
-    document.getElementById("apost").textContent = "Creditos: " + contagem; 
+    AdicionarCredito = document.getElementById("ficha").value;   
+    multCredito = parseInt(AdicionarCredito) + parseInt(CreditoAdicional);
+    document.getElementById("apost1").innerHTML= "Ficha " + AdicionarCredito;
+    document.getElementById("apost").innerHTML = "Creditos: " + multCredito;
+    
+}
+
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    
+    if(display == "none"){
+        document.getElementById(el).style.display = 'block';
+    
+    }else{
+        document.getElementById(el).style.display = 'none';
+    }
 }
 
 function acertou1num(){
-    pontos = pontos + 0.25;
+    pontos = pontos + 0.5;
 }
 
 function acertou2num(){
-    pontos = pontos + 0.5;
-    document.getElementById("pontos").innerHTML = "Pontos = " + pontos;
+    pontos = pontos + 1;
+    document.getElementById("pontos").innerHTML = "Pontos = R$" + pontos;
 }
 
 function acertou3num(){
     pontos = pontos + 1.5;
-    document.getElementById("pontos").innerHTML = "Pontos = " + pontos;
+    document.getElementById("pontos").innerHTML = "Pontos = R$" + pontos;
 }
 
 function setenum(){
-    pontos = pontos * 7;
-    document.getElementById("pontos").innerHTML = "Pontos = " + pontos;
+    pontos = pontos * 0.7;
+    document.getElementById("pontos").innerHTML = "Pontos = R$" + pontos;
+    console.log("777");
 }
 
 function sum(){
     placar++;
-    document.getElementById("jogadas").innerHTML = placar;
+    document.getElementById("jogadas").innerHTML =  "Jogadas:" + placar;
 }
+
 
 function sub(){
     
-    if(contagem>=1){
-        contagem--;
-        document.getElementById("apost").innerHTML = contagem;
-    }
-
-    if(contagem<=0){
-        contagem = 0;
-        document.getElementById("apost").innerHTML = "Creditos: " + contagem;
+  
+    if(multCredito>=1){
+        multCredito--;
+        document.getElementById("apost").innerHTML = "Creditos: " + multCredito;
+        
+    }else{
+        
+        document.getElementById("apost").innerHTML =  "Creditos: " + multCredito;
         alert("acabou os creditos");
+        Mudarestado(el);
     }
        
 }
 
-function jogar(){
-    
-    
-    sub();
-    sum();
+function Update(){
 
-    var a = getRandomInt(7,6);
-    var b = getRandomInt(7,6);
-    var c = getRandomInt(7,6);
 
-    
-   
+    var a = getRandomInt(7,1);
+    var b = getRandomInt(7,1);
+    var c = getRandomInt(7,1);
+
     document.getElementById("A").textContent = "|" + a + "|";
     document.getElementById("B").textContent = "|" + b + "|";
     document.getElementById("C").textContent = "|" + c + "|";
 
     document.getElementById("pontos").textContent = "Pontos = " + pontos;
-    document.getElementById("apost").textContent = "Creditos: " + contagem; 
+    document.getElementById("apost").textContent = "Creditos: " + multCredito; 
     document.getElementById("jogadas").textContent = "Jogadas: " + placar; 
-  
+
+    
+
+    if(multCredito<0){
+        alert("Adicione os Creditos");
+
+    }else{
+
+        if(a!=b || a!=c || b!=c){
+
+            if(pontos>=0){
+                acertou3num();
+                document.getElementById("jogada").textContent = "ACERTOU A B C";
+            }
+
+            if(pontos>=1){
+                    acertou1num();
+                    document.getElementById("jogada").textContent = "PROXIMA TENTATIVA";
+            }
+
+            if(a==b){
+
+                if(pontos>=0){
+                    acertou2num();
+                    document.getElementById("jogada").textContent = "ACERTOU A e B";
+                }
+            }
+
+            if(b==c){
+                if(pontos>=0){
+                    acertou2num();
+                    document.getElementById("jogada").textContent = "ACERTOU B e C";
+                }
+            }
+
+            if(a==c){
+                if(pontos>=0){
+                    acertou2num();
+                    document.getElementById("jogada").textContent = "ACERTOU A e C";
+                }
+            }
        
-    if(a==7&&b==7&&c==7){
+    }else if(a==7&&b==7&&c==7){
         setenum();
     }
 
-
-    if(a!=b || a!=c || b!=c){
-
-        if(pontos>=0){
-            acertou3num();
-            document.getElementById("jogada").textContent = "ACERTOU A B C";
-        }
-
-        if(pontos>=1){
-                acertou1num();
-                document.getElementById("jogada").textContent = "PROXIMA TENTATIVA";
-        }
-
-        if(a==b){
-
-            if(pontos>=0){
-                acertou2num();
-                document.getElementById("jogada").textContent = "ACERTOU A e B";
-            }
-        }
-
-        if(b==c){
-            if(pontos>=0){
-                acertou2num();
-                document.getElementById("jogada").textContent = "ACERTOU B e C";
-            }
-        }
-
-        if(a==c){
-            if(pontos>=0){
-                acertou2num();
-                document.getElementById("jogada").textContent = "ACERTOU A e C";
-            }
-        }
     }
-                
+
 }
+
+function jogar(){
+
+    sub();
+    sum();
+    Update();
+}
+
 
 //Fazer função de add Credito
 
